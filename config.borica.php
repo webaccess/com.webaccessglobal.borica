943ddaa254927a370a0251d6e1d8ad125f947b8d
prathameshsave007 <prathamesh@prathamesh.(none)>
2013-05-31 19:11:54 +0530

    generalize the code and add readme

---
+++ config.borica.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * FIX ME: Change below paths as per your need
+ */
+
+// Path for public key certificate for devlopment, received from BANK
+define('TEST_MODE_PUBLIC_CERT', '/home/prathamesh/public_html/borica/certificate-new/BORICA-Public_test_201212.cer');
+
+// Path for public key certificate for production, received from BANK
+define('PROD_MODE_PUBLIC_CERT', '/home/prathamesh/public_html/borica/certificate-new/BORICA-Public_prod_201212.cer');
+
+// Path for private key for Test environment transactions created by merchant
+define('TEST_MODE_PRIVATE_KEY', '/home/prathamesh/public_html/borica/certificate-new/testborica.clataccess.in.key');
+
+// Path for private key for production environment transactions created by merchant
+define('PROD_MODE_PRIVATE_KEY', '/home/prathamesh/public_html/borica/certificate-new/liveborica.clataccess.in.key');
