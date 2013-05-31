e1c437fd2d3af95f227a047d1a99b99620d6fe90
prathameshsave007 <prathamesh@prathamesh.(none)>
2013-05-31 19:15:47 +0530

    change the default path

--- config.borica.php
+++ config.borica.php
@@ -4,13 +4,13 @@
  */
 
 // Path for public key certificate for devlopment, received from BANK
-define('TEST_MODE_PUBLIC_CERT', '/home/prathamesh/public_html/borica/certificate-new/BORICA-Public_test_201212.cer');
+define('TEST_MODE_PUBLIC_CERT', '/home/htdocs/certificates/BORICA-Public_test_201212.cer');
 
 // Path for public key certificate for production, received from BANK
-define('PROD_MODE_PUBLIC_CERT', '/home/prathamesh/public_html/borica/certificate-new/BORICA-Public_prod_201212.cer');
+define('PROD_MODE_PUBLIC_CERT', '/home/htdocs/certificates/BORICA-Public_prod_201212.cer');
 
 // Path for private key for Test environment transactions created by merchant
-define('TEST_MODE_PRIVATE_KEY', '/home/prathamesh/public_html/borica/certificate-new/testborica.clataccess.in.key');
+define('TEST_MODE_PRIVATE_KEY', '/home/htdocs/certificates/testborica.key');
 
 // Path for private key for production environment transactions created by merchant
-define('PROD_MODE_PRIVATE_KEY', '/home/prathamesh/public_html/borica/certificate-new/liveborica.clataccess.in.key');
+define('PROD_MODE_PRIVATE_KEY', '/home/htdocs/certificates/liveborica.key');
